b6c46c0483aae34ce6f7b4b3fc75a37680063380
alimhtsai <48788292+alimhtsai@users.noreply.github.com>
2024-06-19 20:01:22 -0700

    implement comment section in each post

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,14 +1,30 @@
-import { API, Storage } from "aws-amplify";
+import { API, Auth, Hub, Storage } from "aws-amplify";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import ReactMarkDown from "react-markdown";
 import "../../configureAmplify";
 import { listPosts, getPost } from "../../src/graphql/queries";
 import Moment from 'react-moment';
+import { createComment } from "@/src/graphql/mutations";
+import dynamic from "next/dynamic";
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
+import "easymde/dist/easymde.min.css";
+import { v4 as uuid } from "uuid";
+
+const initialState = { message: "" };
 
 export default function Post({ post }) {
     const router = useRouter();
     const [coverImage, setCoverImage] = useState(null);
+    const [comment, setComment] = useState(initialState);
+    const [showMe, setShowMe] = useState(false);
+    const { message } = comment;
+
+    function toggle() {
+        setShowMe(!showMe);
+    }
 
     useEffect(() => {
         updateCoverImage();
@@ -23,6 +39,23 @@ export default function Post({ post }) {
     
     if (router.isFallback) {
         return <div>Loading...</div>
+    }
+
+    async function createTheComment() {
+        if (!message) return;
+        const id = uuid();
+        comment.id = id;
+
+        try {
+            await API.graphql({
+                query: createComment,
+                variables: { input: comment },
+                authMode: "AMAZON_COGNITO_USER_POOLS"
+            })
+        } catch (error) {
+            console.error(error);
+        }
+        router.push("/my_posts");
     }
 
     return (
@@ -48,6 +81,33 @@ export default function Post({ post }) {
             </div>
             <div className="col-start-2 col-end-6 mt-3">
                 <ReactMarkDown className="prose" children={post.content}/>
+            </div>
+            <div className="col-start-2 col-end-6 mt-3">
+
+            </div>
+            <div className="col-start-2 col-end-6">
+                <button type="button" 
+                        className="btn btn-dark mb-4 font-semibold px-8 py-2 rounded" 
+                        onClick={toggle}>
+                    Write A Comment
+                </button>
+                {
+                    <div 
+                        className="col-start-2 col-end-6"
+                        style={{display: showMe ? "block" : "none"}}>
+                        <SimpleMDE
+                            value={comment.message}
+                            onChange={(value) => 
+                                setComment({...comment, message: value, postID: post.id})
+                            }>
+                        </SimpleMDE>
+                        <button type="button" 
+                                className="btn btn-dark mb-4 font-semibold px-8 py-2 rounded" 
+                                onClick={createTheComment}>
+                            Save
+                        </button>
+                    </div>
+                }
             </div>
         </div>
     )
